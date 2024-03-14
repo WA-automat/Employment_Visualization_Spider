6831601abd1f515b0cb763623aa530f3f06fd96d
WA_automat <1577696824@qq.com>
2024-03-14 10:35:13 +0800

    分类结果上传

--- script/df2data.py
+++ script/df2data.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     df_afterProcessing = pd.read_excel("../data/招聘信息_afterProcessing.xlsx", sheet_name='sheet_1')
     df.to_json("../data/招聘信息汇总.json", orient='records', force_ascii=False)
     df_afterProcessing.to_json("../data/招聘信息_afterProcessing.json", orient='records', force_ascii=False)
+    df_lei = pd.read_excel("../data/分类结果测试.xlsx", sheet_name='sheet_1')
+    df_lei.to_json("../data/分类结果测试.json", orient='records', force_ascii=False)
 
     # 获取不同城市
     citys = list(set(df['site'].values.tolist()))
