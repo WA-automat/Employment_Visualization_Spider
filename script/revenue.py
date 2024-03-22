05664bf68322c51e7d6c2dc79a397bc51b5a248d
WA_automat <1577696824@qq.com>
2024-03-22 08:50:34 +0800

    失业保险与人均可支配收入

--- script/revenue.py
+++ script/revenue.py
@@ -5,15 +5,16 @@ from pmdarima.arima import auto_arima
 if __name__ == '__main__':
     predict_num = 4
     new_data = {}
-    df = pd.read_excel('../data/保险支出.xlsx', sheet_name='Sheet2', index_col=0, header=0)
+    df = pd.read_excel('../data/人均可支配收入.xlsx', sheet_name='Sheet1', index_col=0, header=0)
     name_list = df.columns
     for index in name_list.to_list():
         insurance_data = df[index].values.tolist()
+        print(insurance_data)
         model = auto_arima(insurance_data)
         forcast = model.predict(n_periods=predict_num)
         insurance_data += forcast.tolist()
-        new_items = [list(item) for item in zip(list(range(2006, 2026)), insurance_data)]
+        new_items = [list(item) for item in zip(list(range(2013, 2026)), insurance_data)]
         new_data[index] = new_items
 
-    with open("../data/insurance_2.json", "w", encoding="utf-8") as f:
+    with open("../data/revenue.json", "w", encoding="utf-8") as f:
         json.dump(new_data, f, ensure_ascii=False, indent=4)
